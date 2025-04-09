40d9f1a98f37fc2c7a07ef932c12a7ada606a609
keter0001 <cwan0184@student.monash.edu>
2025-04-09 13:52:23 +1000

    Connect to database and implement data retrieval

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,45 @@
-import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
-import cors from "cors";
-import dotenv from "dotenv";
-import sampleRoutes from "./routes/sampleRoutes.js"
+// import express from "express";
+// import helmet from "helmet";
+// import morgan from "morgan";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import kelpRoutes from "./routes/kelpRoutes.js";  
+// import diveSitesRoutes from "./routes/diveSitesRoutes.js";  
 
-dotenv.config();
+// dotenv.config();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// const PORT = process.env.PORT || 5000;
+
+
+// app.use(helmet());
+// app.use(morgan("dev"));
+
+// app.use('/api/kelp', kelpRoutes);
+// app.use('/api/dive-sites', diveSitesRoutes);
+
+// app.listen(PORT, () => {
+//     console.log("5000");
+// })
+
+import express from 'express';
+import cors from 'cors';
+import kelpRoutes from './routes/kelpRoutes.js';
+import diveSitesRoutes from './routes/diveSitesRoutes.js';
+import newSpeciesRoutes from './routes/newSpeciesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
 app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(express.json());
 
-app.get('/api/sample', sampleRoutes);
 
-app.listen(PORT, () => {
-    console.log("3000");
-})
+
+app.use('/api/kelp', kelpRoutes);
+app.use('/api/dive-sites', diveSitesRoutes);
+app.use('/api/new-species', newSpeciesRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
