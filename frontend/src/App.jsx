fbbc2431261484d393126762850877462a2e442e
VOLIAPOL12 <hwpar5@student.monash.edu>
2025-04-23 11:29:52 +1000

    Login button created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,18 @@ import { Routes, Route } from "react-router-dom";
 
 import React from 'react';
 import './components/KelpMap.css';
+import LoginPage from './pages/authentication/LoginPage';
+import UnauthenticatedPageWrapper from './containers/UnauthenticatedPageWrapper';
 
 
   
   function App() {
     return (
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<UnauthenticatedPageWrapper><HomePage /></UnauthenticatedPageWrapper>} />
         <Route path="/journey" element={<JourneyPage />} />
         <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     );
   }
