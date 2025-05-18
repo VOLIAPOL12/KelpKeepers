537852abb3fe06ae87a15beff2e012c5774b0db1
Junsang Hwang <hwpar5@student.monash.edu>
2025-05-18 19:30:07 +1000

    update

--- backend/controller/dashboardController.js
+++ backend/controller/dashboardController.js
@@ -4,10 +4,8 @@ import { findDashboardData } from "../models/dashboardModel.js";
 export const getDashboardData = async (req, res) => {
     try {
         const {userId} = req.body;
-        console.log(userId);
         
         const result = await findDashboardData(userId)
-        console.log(result.rows);
         res.json({ upcomingDive: result.rows || null });
     } catch (error) {
         res.status(400).json({ success: false, message: error.message})
